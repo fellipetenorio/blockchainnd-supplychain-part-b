6c1d785c215d8d699c6076270372a9dc7ad69695
Fellipe T Ferrer <fellipe.tenorio@gmail.com>
2019-04-06 23:33:55 -0300

    feature: test supplychain
    refatoring functions

--- test/BookSupplyChain.js
+++ test/BookSupplyChain.js
@@ -1,27 +1,33 @@
 var Test = require('./testConfig.js');
 
+const writerName = "ferrer";
+const title = "title";
+const abstract = "abstract";
+const draft = "draft";
+const price = 1;
 
+async function writeAbstractTest(upc, writer, config) {
+    // ASSERT
+    await config.bookSP.writerAbstract(upc, writerName, title, abstract, draft, price, {from: writer});
+}
 
 contract('BookSupplyChain', async (accounts) => {
 
   var config;
+  let owner = accounts[0];
+  let writer = accounts[0];
+  let publisher = accounts[0];
+  let reviewer = accounts[0];
+  let wrilibraryter = accounts[0];
+
   before('setup contract', async () => {
     config = await Test.Config(accounts);
   });
 
   it('contract owner can register new user', async () => {
-    let writer = accounts[0];
-    let newUser = config.testAddresses[0]; 
     let upc = 1;
-    let writerName = "ferrer";
-    let title = "title";
-    let abstract = "abstract";
-    let draft = "draft";
-    let price = 1;
+    await writeAbstractTest(upc, writer, config);
 
-    // ASSERT
-    await config.bookSP.writerAbstract(upc, writerName, title, abstract, draft, price,
-        {from: writer});
     let fetchedBook = await config.bookSP.fetchBook.call(upc);
     console.log('book', fetchedBook);
     assert.equal(fetchedBook.writerName, writerName, "Invalid book writer name");
